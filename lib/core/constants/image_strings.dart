418e285ae36f0ffd473b85edb5ecf980e20187c3
hichamid4 <hicham.idhmed20@gmail.com>
2025-02-27 22:26:33 +0000

    Add login screen and related components; implement login form, header, and social buttons

--- lib/core/constants/image_strings.dart
+++ lib/core/constants/image_strings.dart
@@ -1,11 +1,11 @@
 class TImages {
   // -- App Logos
-  static const String darkAppLogo = 'assets/logos/g-spalsh-dark-logo.png';
+  static const String darkAppLogo = 'assets/logos/logo.png';
   static const String lightAppLogo = 'assets/logos/g-spalsh-light-logo.png';
 
   // -- Social logos
-  static const String facebook = 'assets/logos/facebook-icon.png';
-  static const String google = 'assets/logos/google-icon.png';
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
 
   // -- Onboarding
   static const String onboarding1 = 'assets/onboardings/onboarding1.png';
